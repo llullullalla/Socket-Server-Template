692494ca09066f9d2af8b89b0461600abeb26b6c
llullullalla <jeainpyo@gmail.com>
2025-02-16 10:02:46 +0100

    Create lesson_03_turtle.py

---
+++ lesson_03_turtle.py
@@ -0,0 +1,23 @@
+from turtle import *
+
+speed(1)
+bgcolor("blue")
+
+penup()
+goto(-150, -100)
+fillcolor("yellow")
+begin_fill()
+pendown()
+
+forward(300)
+left(120)
+forward(300)
+left(120)
+forward(300)
+left(120)
+
+end_fill()
+
+hideturtle()
+done()  
+
