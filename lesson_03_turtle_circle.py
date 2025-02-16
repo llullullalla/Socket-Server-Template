771ff5067d15feca542ae98473a9d48e63ce73d8
llullullalla <jeainpyo@gmail.com>
2025-02-16 09:59:32 +0100

    Create lesson_03_turtle_circle.py

---
+++ lesson_03_turtle_circle.py
@@ -0,0 +1,17 @@
+from turtle import *
+
+speed(0)
+penup()
+goto(0, -100)
+bgcolor("yellow")
+color("magenta", "cyan")
+pensize(10)
+
+
+pendown()
+begin_fill()
+circle(120)
+end_fill()
+
+hideturtle()
+exitonclick()
